4f4917a34f2ac9a94815a3c89e600e3eb701ec26
realzhaoyanpeng <73323823+realzhaoyanpeng@users.noreply.github.com>
2020-11-01 22:18:32 +0800

    Update test.php

--- example/test.php
+++ example/test.php
@@ -1,6 +1,7 @@
 <?php
 
-include __DIR__ . "./src/classb.php";
+include_once  __DIR__ . "../src/tcmeeting/classb.php";
+include_once __DIR__ . "../src/tcmeeting/classa.php";
 
 use tcmeeting\classa;
 use tcmeeting\classb;
